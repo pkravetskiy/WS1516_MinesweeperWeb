a63471d2e06ab150642cfdae8b1ab9d1f2682360
Pavel Kravetskiy <pavkrave@htwg-konstanz.de>
2015-11-18 13:26:50 +0100

    on the way to implement javascript and json support

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -12,11 +12,15 @@ import de.htwg.se.model.impl.Field;
 import de.htwg.se.util.observer.Event;
 import de.htwg.se.util.observer.IObserver;
 import views.html.*;
+import play.libs.Json;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Application extends Controller {
 	private Minesweeper minesweeper = Minesweeper.getInstance();
 	private IController controller = minesweeper.getTui().getController();
   public Result index() {
+		System.out.println(json());
     return ok(views.html.index.render("Minesweeper", controller));
   }
 
@@ -42,5 +46,31 @@ public class Application extends Controller {
 
 	public Result authors() {
 		return ok(views.html.authors.render());
+	}
+
+	public Result jsonCommand(String command) {
+			minesweeper.getTui().processInputLine(command);
+			return json();
+	}
+
+	public Result json() {
+		int x = controller.getPlayingField().getLines();
+		int y = controller.getPlayingField().getColumns();
+		Map<String, Object> field[][] = new HashMap[x][y];
+
+		for (int i=0; i < x; i++) {
+			for (int j=0; j < y; j++) {
+				field[i][j] = new HashMap<String, Object>();
+				field[i][j].put("value", new Integer(controller.getPlayingField().getCell(i, j).getValue()));
+				field[i][j].put("isrevealed", new Boolean(controller.getPlayingField().getCell(i, j).isRevealed()));
+			}
+		}
+
+		Map<String, Object> json = new HashMap<String, Object>();
+		json.put("victory", new Boolean(controller.isVictory()));
+		json.put("loose", new Boolean(controller.isGameOver()));
+		json.put("field", field);
+
+		return ok(Json.stringify(Json.toJson(json)));
 	}
 }
