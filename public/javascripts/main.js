b9f30595c33bc5dfe1fd1237f8870341d8ccfef2
Pavel Kravetskiy <pavkrave@htwg-konstanz.de>
2015-11-25 15:35:14 +0100

    implemented buttons disabling

---
+++ public/javascripts/main.js
@@ -0,0 +1,24 @@
+function fill_grid(x, y, jsonData) {
+  var obj = JSON.parse(jsonData);
+  field = obj.get("field");
+  for (i=0; i < x; i++) {
+    for (j=0; j < y; j++) {
+      var value = "";
+      switch (field[i][j].value) {
+        case -1:
+          value = "M";
+          break;
+        case 0:
+          value = "";
+          break;
+        default:
+          value = field[i][j].value;
+      }
+      document.getElementById(i + "-" + j).textContent = value;
+    }
+  }
+}
+
+function click(x, y) {
+  
+}
