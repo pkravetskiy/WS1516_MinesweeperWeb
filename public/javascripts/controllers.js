513b9a80c599aeb065f23706088b12e748cb0817
D-Hahn <nonPublic@nonPublic.com>
2016-01-03 20:44:26 +0100

    Fix

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -2,6 +2,6 @@ var app = angular.module("minesweeperApp", []);
 
 app.controller('fieldCtrl', function ($scope, $http) {
   $http.get('/json').success(function(data) {
-    $scope.phones = data;
+    $scope.playingField = data;
   });
 });
